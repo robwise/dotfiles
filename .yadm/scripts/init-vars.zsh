5c19b8e8f30604ec85059112f806c50a9b65ec38
Rob Wise <rwise914@gmail.com>
2018-08-24 01:21:45 -0400

    Change git email address

--- .yadm/scripts/init-vars.zsh
+++ .yadm/scripts/init-vars.zsh
@@ -2,7 +2,7 @@
 GIT_AUTHOR_NAME="Rob Wise"
 GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
 git config --global user.name "$GIT_AUTHOR_NAME"
-GIT_AUTHOR_EMAIL="robert.wise@outlook.com"
+GIT_AUTHOR_EMAIL="rwise914@gmail.com"
 GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
 git config --global user.email "$GIT_AUTHOR_EMAIL"
 
